34eedf7fd74ef80a6b5c0ca00235139c7a0dd8d1
Audreywang27 <114363498+Audreywang27@users.noreply.github.com>
2025-05-30 16:25:53 -0400

    This is me trying to make test methods for us

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Hello World")
+import random
+
+def alt():
+    altitude = random.randint(0,10000)
+    print(f"Altitude: {altitude} feet")
+    return altitude
+
+def temp():
+    temperature = random.randint(-60,100)
+    print(f"Temperature: {temperature}*C")
+    return temperature
+
+def gps():
+    lattitude = random.uniform(20.0,50.0)
+    longitude = random.uniform(-180.0,180.0)
+    print(f"Lattitude = {lattitude}")
+    print(f"Longitude = {longitude}")
+    return lattitude,longitude
+
+alt()
+temp()
+gps()
