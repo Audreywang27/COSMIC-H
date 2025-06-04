8249b6ccee51ef1b6c835d15dd7814ab23e42971
Audreywang27 <114363498+Audreywang27@users.noreply.github.com>
2025-06-04 16:21:51 -0400

    Added humidity function, got altitude code from online - going to delete later...it's just way better than my old code bc it goes up and has unpredictable spikes and drops in the altitude, kind of like the real balloon flying.

--- main.py
+++ main.py
@@ -1,79 +1,109 @@
 import time
+import threading
 import random
 import csv
 import datetime
 import pytz
 
-height = 0 #Altitude in feet
-counter = 0
-countUp = 1
+#Simulates sensor data
+#-----------------------------------------------------------------------------------------------
+#The code generated below is by Chat GPT, simulating a very random altitude increase/decrease 
 
-while True:
-    if height > 13:
-        countUp = 0
+current_altitude = 0.0
 
-    if height <= 0 and counter >  1:
-        break
+def simulate_altitude():
+    global current_altitude
+    rising = True
 
-    if height < 10:
-        counter = counter + 1
+    while True:
+        if rising:
+            # Simulate slow rise with small randomness
+            current_altitude += random.uniform(0.5, 1.5)
+            if current_altitude > 30000:  # simulate max altitude (30,000 ft)
+                rising = False
+        else:
+            # Simulate slow descent
+            current_altitude -= random.uniform(0.5, 1.5)
+            if current_altitude < 0:
+                current_altitude = 0
+                rising = True  # loop back for fun
 
-    if countUp == 1 and height <=20:
-        height = height + 1
-    elif countUp == 0:
-        height = height - 1
-    
-    print(height)
-    #Simulates sensor data
-    def alt():
-        altitude = random.randint(0,10000)
-        #print(f"Altitude: {altitude} feet")
-        return altitude
+        #print(f"Simulated Altitude: {round(current_altitude, 2)} ft")
+        time.sleep(0.5)  # Adjust speed of simulation
+# Returns the latest altitude (used by logger)
+def get_altitude():
+    return round(current_altitude, 2)
+#-----------------------------------------------------------------------------------------
+'''
+def altcalc():
+    global alt_history
 
-    def temp():
-        temperature = random.randint(-60,100)
-        #print(f"Temperature: {temperature}*C")
-        return temperature
+    while len(alt_history) == 0:
+        time.sleep(0.1)
 
-    def latt():
-        lattitude = random.uniform(20.05,50.0)
-        #print(f"Lattitude = {lattitude}")
-        return lattitude
+    maxAlt = alt_history[0]
+    while True:
+        for i in range(0, len(alt_history)):
+            if maxAlt < alt_history[i]:
+                maxAlt = alt_history[i]
+            elif maxAlt > alt_history[i]:
+                print(f"Max reached at {maxAlt}")
+                return
 
-    def long():
-        longitude = random.uniform(-180.0,180.0)
-        #print(f"Longitude = {longitude}")
-        return longitude
+'''
+def temp():
+    temperature = random.randint(-60,100)
+    #print(f"Temperature: {temperature}*C")
+    return temperature
 
-    def timeStamp():
-        dt_utcnow = datetime.datetime.now(tz = pytz.UTC)
-        dt_est = dt_utcnow.astimezone(pytz.timezone("US/Eastern"))
-        #print(f"Time Stamp: {dt_est}")
-        return dt_est
+def latt():
+    lattitude = random.uniform(20.05,50.0)
+    #print(f"Lattitude = {lattitude}")
+    return lattitude
 
-    alt()
-    temp()
-    latt()
-    long()
-    timeStamp()
+def long():
+    longitude = random.uniform(-180.0,180.0)
+    #print(f"Longitude = {longitude}")
+    return longitude
+
+def timeStamp():
+    dt_utcnow = datetime.datetime.now(tz = pytz.UTC)
+    dt_est = dt_utcnow.astimezone(pytz.timezone("US/Eastern"))
+    #print(f"Time Stamp: {dt_est}")
+    return dt_est
+
+def humid():
+    humiditygenerator = random.uniform(0,100) # going to get rid of this eventually
+    #print(f"The humidity is {humiditygenerator}%")
+    return humiditygenerator
 
 
-    #Simulates Image Data
+#Stimulates storing the data to a CSV file
+def logger():
 
-    #Stimulates storing the data to a CSV file
-    #Header = Altitude, Temperature, Coordinates(tuple)
-    dataSet = [
-        {"Time Stamp": timeStamp(), "Altitude" : alt(), "Temperature" : temp(), "Lattitude" : latt(), "Longitude" : long()}
-    ]
+    while True:
+        dataSet = [
+            {"Time Stamp": timeStamp(), "Altitude" : get_altitude(), "Temperature" : temp(), "Humidity" : humid(), "Lattitude" : latt(), "Longitude" : long()}
+        ]
 
-    with open("data.csv", mode = "a", newline = "") as csvfile:
-        fieldnames = ["Time Stamp", "Altitude", "Temperature", "Lattitude", "Longitude"]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writerows(dataSet)
+        with open("data.csv", mode = "a", newline = "") as csvfile:
+            fieldnames = ["Time Stamp", "Altitude", "Temperature", "Humidity", "Lattitude", "Longitude"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writerows(dataSet)
 
-    print("hello")
-    time.sleep(1)
+        time.sleep(1)
 
+
+#Threading
+
+t1 = threading.Thread(target = simulate_altitude)
+t2 = threading.Thread(target = logger)
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
 
 
 
